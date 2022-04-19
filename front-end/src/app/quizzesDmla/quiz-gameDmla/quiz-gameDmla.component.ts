4987eda611b3de956fa4a1a536a1a0fef34b0956
habib belmokhtar <habib.belmokhtar@etu.unice.fr>
2022-04-19 11:54:18 +0200

    amelioration

--- front-end/src/app/quizzesDmla/quiz-gameDmla/quiz-gameDmla.component.ts
+++ front-end/src/app/quizzesDmla/quiz-gameDmla/quiz-gameDmla.component.ts
@@ -18,16 +18,16 @@ export class QuizGameDmlaComponent implements OnInit {
 
   public currentQuestion: number;
   public answerSelected: number;
-  // public correctAnswers;
+  public correctAnswers: AnswerDmla;
   // public incorrectAnswers;
   public quizGameToCreate: QuizGameDmla;
-  public result = false;
   public quiz: QuizDmla;
   public game: QuizGameDmla;
   public quizGameForm: FormGroup;
   public listAnswer = ['A', 'B', 'C', 'D'];
   public index;
   public showQuestion: boolean;
+  public showAnswer: boolean;
 
   constructor(public formBuilder: FormBuilder, private route: ActivatedRoute, private quizService: QuizServiceDmla) {
     this.quizService.quizSelected$.subscribe((quiz) => {
@@ -54,10 +54,25 @@ export class QuizGameDmlaComponent implements OnInit {
     this.quizService.addQuizGame(this.quizGameToCreate);
     this.showQuestion = true;
     this.navigate();
+    this.correctAnswer(this.quiz.questions[this.currentQuestion]);
+    this.showAnswer = false;
   }
 
-  showResult(): void {
-    this.result = true;
+
+  correctAnswer(options: QuestionDmla): void {
+    if (options.answers.length > 0 && options.answers[0].isCorrect) {
+      this.correctAnswers = options.answers[0];
+
+      if (options.answers.length > 1 && options.answers[1].isCorrect) {
+        this.correctAnswers = options.answers[1];
+      }
+      if (options.answers.length > 2 && options.answers[2].isCorrect) {
+        this.correctAnswers = options.answers[3];
+      }
+      if (options.answers.length > 3 && options.answers[3].isCorrect) {
+        this.correctAnswers = options.answers[3];
+      }
+    }
   }
 
   answerSelectedForClass(i, nb: number, option: AnswerDmla): void {
@@ -73,27 +88,42 @@ export class QuizGameDmlaComponent implements OnInit {
       bouttonAfter.remove('bouton');
       bouttonAfter.add('boutonselected');
       this.answerSelected = nb;
-      this.readAnswer(this.quiz.questions[this.currentQuestion].answers[this.answerSelected]);
+      // this.readAnswer(this.quiz.questions[this.currentQuestion].answers[this.answerSelected]);
       // this.readAnswer(option);
     } else {
       setTimeout(() => {
         this.currentQuestion++;
         this.answerSelected = 0;
         this.showQuestion = true;
+        if (this.currentQuestion >= this.quiz.questions.length){
+          this.showAnswer = true;
+        }
       }, 2000);
 
       if (option.isCorrect) {
         const classList = linkbuttons[this.answerSelected].classList;
         classList.remove('boutonselected');
         classList.add('boutonselectedTrue');
+        const synth = window.speechSynthesis;
+        const utterThis = new SpeechSynthesisUtterance('Bonne réponse');
+        utterThis.lang = 'fr-FR';
+        synth.speak(utterThis);
         this.quizGameToCreate.correctAnswers = String(Number(this.quizGameToCreate.correctAnswers) + 1);
+
       } else {
         const classList = linkbuttons[this.answerSelected].classList;
         classList.remove('boutonselected');
         classList.add('boutonselectedFalse');
+        const synth = window.speechSynthesis;
+        const utterThis = new SpeechSynthesisUtterance('Mauvaise réponse');
+        utterThis.lang = 'fr-FR';
+        synth.speak(utterThis);
         this.quizGameToCreate.incorrectAnswers = String(Number(this.quizGameToCreate.incorrectAnswers) + 1);
       }
     }
+    console.log(this.currentQuestion);
+    console.log('sup +=');
+    console.log(this.quiz.questions.length);
   }
 
   navigate(): void {
@@ -172,17 +202,19 @@ export class QuizGameDmlaComponent implements OnInit {
           console.log('JE SUIS Space');
           this.readAnswerQuestion(this.quiz.questions[this.currentQuestion]);
           console.log('touche', nomTouche);
+        } else if (this.showAnswer === true) {
+          console.log('JE SUIS OIXEL"=');
         } else {
           console.log('JE SUIS Space');
           this.readAnswer(this.quiz.questions[this.currentQuestion].answers[this.answerSelected]);
           console.log('touche', nomTouche);
         }
-
       }
     }, true);
   }
 
   readAnswer(option: AnswerDmla): void {
+    console.log('Read answer', option.value);
     const synth = window.speechSynthesis;
     const utterThis = new SpeechSynthesisUtterance(option.value);
     utterThis.lang = 'fr-FR';
@@ -190,6 +222,7 @@ export class QuizGameDmlaComponent implements OnInit {
   }
 
   readAnswerQuestion(option: QuestionDmla): void {
+    console.log('Read question', option.label);
     const synth = window.speechSynthesis;
     const utterThis = new SpeechSynthesisUtterance(option.label);
     utterThis.lang = 'fr-FR';
@@ -199,4 +232,6 @@ export class QuizGameDmlaComponent implements OnInit {
   passQuestion(): void {
     this.showQuestion = false;
   }
+
+
 }
