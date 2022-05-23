9b5d5ee8720cd44e41087ebcc0a9271a6447fed2
Tom Bevan <bevantom@protonmail.com>
2022-05-23 15:50:29 +0200

    Change back :)

--- back-end/app/models/quizGameDmla.model.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi')
-const BaseModel = require('../utils/base-model.js')
-
-module.exports = new BaseModel('QuizGameDmla', {
-    correctAnswers: Joi.string().required(),
-    incorrectAnswers: Joi.string().required(),
-    quiz: Joi.string().required(),
-})
